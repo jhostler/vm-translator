98e832504da05952465b663d0dad1e219705f9a9
condraversial <condraversial@gmail.com>
2016-04-03 17:39:38 -0500

    Added files via upload

---
+++ vmTranslator.java
@@ -0,0 +1,14 @@
+ public class vmTranslator {
+	
+	public static void main(String[] args) {
+
+        if (args.length == 0){
+
+            System.out.println("Usage: Assembler filename");
+            return;
+
+        }        
+    }
+	
+	
+}
